8dd0518918fa951a321995653ff6cbcbe96a34ce
samiarn845 <samiranmaity845@gmail.com>
2023-06-26 11:17:38 +0530

    first animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Lottie from "lottie-react";
+import animationData from "./Animetion-Json/97525-code-dark.json"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Create a Love Animation with Samiran Maity</h2>
+      <p>This is made by Samiran Maity</p>
+      <div className="animetionHolder">
+        <Lottie animationData={animationData} />
+      </div>
     </div>
   );
 }
